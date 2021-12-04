e7dc958e3e72f4d060cf96a06b2f12e502e8eead
Mike Solomon <mike@wavr.so>
2021-12-04 17:57:39 +0200

    Adds back cache

--- src/JIT/EvalSources.purs
+++ src/JIT/EvalSources.purs
@@ -6,9 +6,15 @@ import Effect (Effect)
 import Foreign (Foreign)
 import Foreign.Object (Object)
 
-foreign import evalSources_ :: Object String -> String -> Effect Foreign
+data Modules
+
+type EvalResult = { evaluated :: Foreign, modules :: Modules }
+
+foreign import freshModules :: Effect Modules
+
+foreign import evalSources_ :: Modules -> Object String -> String -> Effect EvalResult
 
 fileTag = "<file>" :: String
 
-evalSources :: Object String -> Effect Foreign
-evalSources = flip evalSources_ fileTag
+evalSources :: Modules -> Object String -> Effect EvalResult
+evalSources mod os = evalSources_ mod os fileTag
--- test/Main.purs
+++ test/Main.purs
@@ -8,11 +8,11 @@ import Data.Maybe (Maybe(..))
 import Effect (Effect)
 import Effect.Aff (Milliseconds(..), error, launchAff_, makeAff)
 import Effect.Class (liftEffect)
-import JIT.Compile (compile)
 import Foreign (Foreign)
 import Foreign.Index (readProp)
+import JIT.Compile (compile)
+import JIT.EvalSources (evalSources, freshModules)
 import JIT.Loader (makeLoader)
-import JIT.EvalSources (evalSources)
 import Simple.JSON as JSON
 import Test.Spec (describe, it)
 import Test.Spec.Assertions (shouldEqual)
@@ -20,10 +20,12 @@ import Test.Spec.Reporter (consoleReporter)
 import Test.Spec.Runner (defaultConfig, runSpec')
 import Unsafe.Coerce (unsafeCoerce)
 
-loaderUrl= "https://purescript-wags.netlify.app/js/output" :: String
-compileUrl= "https://supvghemaw.eu-west-1.awsapprunner.com" :: String
+loaderUrl = "https://purescript-wags.netlify.app/js/output" :: String
+compileUrl = "https://supvghemaw.eu-west-1.awsapprunner.com" :: String
+
 main :: Effect Unit
 main = do
+  fm <- freshModules
   launchAff_
     $ runSpec' (defaultConfig { timeout = Just (Milliseconds 10_000.0) }) [ consoleReporter ] do
         describe "jit" do
@@ -45,9 +47,8 @@ add42 = add 42
                 , successCallback: Right >>> cb
                 }
               mempty
-            add42' <- liftEffect $ evalSources success.js
-              >>= runExceptT <<< readProp "add42"
-              >>= either (throwError <<< error <<< show) pure
+            add42'' <- liftEffect $ evalSources fm success.js
+            add42' <- (runExceptT $ readProp "add42" add42''.evaluated) >>= either (throwError <<< error <<< show) pure
             let add42 = (unsafeCoerce :: Foreign -> Int -> Int) add42'
             43 `shouldEqual` add42 1
             success2 <- makeAff \cb -> do
@@ -66,8 +67,7 @@ add24 = add 24
                 , successCallback: Right >>> cb
                 }
               mempty
-            add24' <- liftEffect $ evalSources success2.js
-              >>= runExceptT <<< readProp "add24"
-              >>= either (throwError <<< error <<< show) pure
+            add24'' <- liftEffect $ evalSources add42''.modules success2.js
+            add24' <- (runExceptT $ readProp "add24" add24''.evaluated) >>= either (throwError <<< error <<< show) pure
             let add24 = (unsafeCoerce :: Foreign -> Int -> Int) add24'
             25 `shouldEqual` add24 1
